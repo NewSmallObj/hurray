4892c36bd1d60fd9ae9285a3371f116d9ad4ff6c
makaiyuan123 <912589025@qq.com>
2024-10-15 23:55:03 +0800

    修改布局

--- src/app/admin/adm/layout.tsx
+++ src/app/admin/adm/layout.tsx
@@ -2,11 +2,13 @@
 import useCollapse from '@/app/store/useCollapse'
 import useTheme from '@/app/store/useTheme'
 import { themeValues } from '@/app/utils/themeStants'
-import { ConfigProvider, Layout, theme, ThemeConfig } from 'antd'
+import { Breadcrumb, ConfigProvider, Layout, Splitter, theme, ThemeConfig } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 import { useEffect, useMemo } from 'react'
 const { Header, Footer, Sider, Content } = Layout
 import clsx from "clsx"
+import { useRouter } from 'next/router'
+import { usePathname, useSelectedLayoutSegment, useSelectedLayoutSegments } from 'next/navigation'
 
 export default function RootLayout({
 	children,
@@ -21,24 +23,53 @@ export default function RootLayout({
 
 	const { currentTheme } = useTheme()
 
+  const pathname = usePathname()
+
+  // const items = 
+
   const algorithm = useMemo(() => {
     return currentTheme === 'dark' ? theme.darkAlgorithm : theme.defaultAlgorithm
   }, [currentTheme])
 
 	return (
-		<ConfigProvider theme={{
-      algorithm,
-      token: themeValues[currentTheme].token
-    }} locale={zhCN}>
-			<Layout className="w-full h-full box-border">
-				{header}
+		<ConfigProvider 
+      componentSize='middle'
+      theme={{
+        algorithm,
+        token: themeValues[currentTheme].token
+      }} 
+      locale={zhCN}>
+			
+      <Splitter layout="vertical" className='w-full h-full box-border'>
+        <Splitter.Panel max={50} defaultSize={50}>
+          {header} 
+        </Splitter.Panel>
+        <Splitter.Panel>
+          <Splitter layout="horizontal">
+            <Splitter.Panel defaultSize={200} max={200}>
+              {sider}
+            </Splitter.Panel>
+            <Splitter.Panel>
+              
+              <div className='w-full h-full p-[20px] box-border'>
+                {children}
+              </div>
+            </Splitter.Panel>
+          </Splitter>
+        </Splitter.Panel>
+      </Splitter>
+		</ConfigProvider>
+	)
+}
+
+
+
+{/* <Layout className="w-full h-full box-border">
+				
 				<Layout>
 					<Sider theme="light" collapsed={collapse} width="200px">
 						{sider}
 					</Sider>
 					<Content className='w-full h-full p-[20px] box-border'>{children}</Content>
 				</Layout>
-			</Layout>
-		</ConfigProvider>
-	)
-}
+			</Layout> */}
